7253cc4951b2351f0f181ecd8b4fe7262b22a8b1
Web Development Services <52756530+zion3w@users.noreply.github.com>
2025-10-24 09:35:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,28 +2,28 @@ var CACHE_NAME = 'v1';
 var urlsToCache = [
 '/manifest.json',
 // '/min.css',
-'/js/min.js',
+'/_js/min.js',
 // additional assets
 // fonts
-'/fonts/roboto/roboto.woff2',
-'/fonts/icons.woff2',
+'/_fonts/roboto/roboto.woff2',
+'/_fonts/icons.woff2',
 // favicon
 '/favicon.ico',
 '/favicon.png',
-'/img/favicon/favicon32.png',
-'/img/favicon/favicon48.png',
-'/img/favicon/favicon62.png',
-'/img/favicon/favicon72.png',
-'/img/favicon/favicon96.png',
-'/img/favicon/favicon144.png',
-'/img/favicon/favicon152.png',
-'/img/favicon/favicon167.png',
-'/img/favicon/favicon180.png',
-'/img/favicon/favicon192.png',
-'/img/favicon/favicon512.png',
+'/_img/favicon/favicon32.png',
+'/_img/favicon/favicon48.png',
+'/_img/favicon/favicon62.png',
+'/_img/favicon/favicon72.png',
+'/_img/favicon/favicon96.png',
+'/_img/favicon/favicon144.png',
+'/_img/favicon/favicon152.png',
+'/_img/favicon/favicon167.png',
+'/_img/favicon/favicon180.png',
+'/_img/favicon/favicon192.png',
+'/_img/favicon/favicon512.png',
 // images
-// '/img/textures/square.png',
-// '/img/logo.svg'
+// '/_img/textures/square.png',
+// '/_img/logo.svg'
 ];
 self.addEventListener('install', function(event) {
 	event.waitUntil(
