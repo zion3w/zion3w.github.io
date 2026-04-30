5723dab9d5371c3cbbd12f10c5333460c338482d
Web Development Services <52756530+zion3w@users.noreply.github.com>
2026-04-30 15:38:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,61 +1,64 @@
-var CACHE_NAME = 'v1';
+var CACHE_NAME = '260330';
 var urlsToCache = [
-'/manifest.json',
-// '/min.css',
-'/_js/min.js',
-// additional assets
+'/min.css',
+'/min.js',
 // fonts
-'/_fonts/roboto/roboto.woff2',
-'/_fonts/icons.woff2',
+'/a/fonts/roboto/roboto.woff2',
+'/a/fonts/opensans/opensans.woff2',
+'/a/fonts/icons.woff2',
+'/a/fonts/matrix.woff2',
 // favicon
-'/favicon.ico',
-'/favicon.png',
-'/_img/favicon/favicon32.png',
-'/_img/favicon/favicon48.png',
-'/_img/favicon/favicon62.png',
-'/_img/favicon/favicon72.png',
-'/_img/favicon/favicon96.png',
-'/_img/favicon/favicon144.png',
-'/_img/favicon/favicon152.png',
-'/_img/favicon/favicon167.png',
-'/_img/favicon/favicon180.png',
-'/_img/favicon/favicon192.png',
-'/_img/favicon/favicon512.png',
+'/img/fav_z3w/192.png',
+'/img/fav_z3w/512.png',
 // images
-// '/_img/textures/square.png',
-// '/_img/logo.svg'
 ];
 self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open(CACHE_NAME).then(function(cache) {
-			return cache.addAll(urlsToCache);
-		})
-		);
+event.waitUntil(
+caches.open(CACHE_NAME).then(function(cache) {
+return cache.addAll(urlsToCache);
+})
+);
 });
+
+const deleteCache = async (key) => {
+await caches.delete(key);
+};
+const deleteOldCaches = async () => {
+const cacheKeepList = [CACHE_NAME];
+const keyList = await caches.keys();
+const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+await Promise.all(cachesToDelete.map(deleteCache));
+};
+self.addEventListener("activate", (event) => {
+event.waitUntil(deleteOldCaches());
+});
+
+
 var siteurl = self.location.hostname;
 // function addToCache(cacheName, request, response) {
 // caches.open(cacheName)
 // .then( cache => cache.put(request, response) );
 // }
+
 self.addEventListener('fetch', function(event) {
-	if (event.request.url.indexOf(siteurl) !== -1) {
-		event.respondWith(
-			caches.match(event.request).then(function(response) {
-				if (response) {
-					return response;
-				}
-				return fetch(event.request).then(function(response) {
-					// @todo: addToCache default cats images + make 2 caches: for assets and images
-					// if (event.request.url.indexOf(siteurl+'/images/default/') !== -1) {
-					// console.log(event.request.url);
-					// addToCache('img', event.request, response.clone());
-					// }
-					return response;
-				}).catch(function(error) {
-					console.error('Fetching failed: ', error);
-					throw error;
-				});
-			})
-			);
-	}
+if (event.request.url.indexOf(siteurl) !== -1) {
+event.respondWith(
+caches.match(event.request).then(function(response) {
+if (response) {
+return response;
+}
+return fetch(event.request).then(function(response) {
+// @todo: addToCache default cats images + make 2 caches: for assets and images
+// if (event.request.url.indexOf(siteurl+'/images/default/') !== -1) {
+// console.log(event.request.url);
+// addToCache('img', event.request, response.clone());
+// }
+return response;
+}).catch(function(error) {
+console.error('Fetching failed: ', error);
+throw error;
+});
+})
+);
+}
 });
